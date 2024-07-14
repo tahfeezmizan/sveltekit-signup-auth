5928cb0649435d611b3e3cc803f394fc40470cc4
tahfeezmizan <mizanahmed5316@gmail.com>
2024-07-14 23:03:44 +0600

    properly install svelte-kit

--- client-side/src/Hook/UseAuth.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
-
-const UseAuth = () => {
-    const contextData = useContext(AuthContext);
-    return contextData;
-};
-
-export default UseAuth;
